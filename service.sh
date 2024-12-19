844da0d7273c0ecb649c9ab20c648b31e831339f
EndrewSilva <endrew.developer@gmail.com>
2024-12-19 13:41:09 -0300

    finally

--- service.sh
+++ service.sh
@@ -1,1 +1,43 @@
-docker-compose up --build
+#!/bin/bash
+
+docker_create() {
+  docker compose build --no-cache
+  sudo systemctl stop systemd-resolved
+  echo "Criando containers e configurando a infraestrutura..."
+  docker compose up -d --build
+  echo "Infraestrutura criada com sucesso!"
+}
+
+docker_start() {
+  echo "Iniciando os serviços..."
+  docker compose start
+  echo "Serviços iniciados!"
+}
+
+docker_stop() {
+  echo "Parando os serviços..."
+  docker compose stop
+  echo "Serviços parados!"
+  echo "Removendo containers e limpando a infraestrutura..."
+  docker compose down --volumes
+  echo "Infraestrutura removida com sucesso!"
+  echo "Removendo todas as imagens Docker..."
+  docker rmi -f $(docker images -q)
+  echo "Todas as imagens foram removidas!"
+}
+
+case "$1" in
+  create)
+    docker_create
+    ;;
+  start)
+    docker_start
+    ;;
+  stop)
+    docker_stop
+    ;;
+    *)
+    echo "Uso: $0 {create|start|stop}"
+    exit 1
+    ;;
+esac
